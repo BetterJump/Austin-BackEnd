d49a9f92ab637083f024a3655586d9440bfc57d6
Austin Stotts <austintstotts@gmail.com>
2019-01-15 15:59:29 -0600

    test database query times

--- database-tests.js
+++ database-tests.js
@@ -1,23 +1,35 @@
-const pg = require('pg');
+//const pg = require('pg');
 
-const client = new pg.Client({
-  user: 'ubuntu',
-  host: '127.0.0.1',
-  database: 'jump',
-  port: 5432,
-})
+// const client = new pg.Client({
+//   user: 'aust',
+//   password: '00000000',
+//   host: '127.0.0.1',
+//   database: 'jump',
+//   port: 5432,
+// })
 
-const one = function () {
-  console.time();
-  client.query('SELECT * FROM comments WHERE id = 5000000')
-  .then((results) => {
-    console.timeEnd();
-    console.log(results);
-  })
-  .catch((err) => {
-    console.timeEnd();
-    console.log(err);
-  });
-};
+// async function one () {
+//   client.connect()
+//   client.query('SELECT * FROM comments WHERE id = 5000000')
+//   .then((results) => {
+//     return console.timeEnd();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     return console.timeEnd();
+//   });
+// };
 
-one();
+// let data = [];
+let x = Date.now();
+
+for(let i = 0; i < 30000000; i++) {
+  // let x = await one();
+  // data.push(x);
+}
+
+let y = Date.now();
+
+console.log((y-x)/100);
+
+//console.log(data.reduce((a, b) => a + b, 0) / data.length);
